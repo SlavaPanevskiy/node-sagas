9dc7c28bd37f2153643814f5a79e8c29c2696545
Viacheslav.Panevskyi <amangions@gmail.com>
2020-04-29 21:08:07 +0300

    Refactor package structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { SagaCompensationFailed, SagaExecutionFailed } from './exceptions';
-export { Saga, SagaStates } from './saga';
-export { SagaBuilder } from './saga-builder';
+export { SagaCompensationFailed, SagaExecutionFailed } from './lib/exceptions';
+export { Saga, SagaStates } from './lib/saga';
+export { SagaBuilder } from './lib/saga-builder';
